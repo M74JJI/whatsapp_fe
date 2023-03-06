4de987afd6a3895bc0400f171ca59c2185ba633f
MOHAMED HAJJI <pytaek1@gmail.com>
2023-03-06 14:34:32 +0100

    adding react router dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+//Pages
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
 function App() {
-  return <div className="dark"></div>;
+  return (
+    <div className="dark">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
