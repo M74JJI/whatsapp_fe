20073055ddfc3b1721158bfbda06ff699579a3a8
MOHAMED HAJJI <pytaek1@gmail.com>
2023-03-06 18:42:17 +0100

    adding redux toolkit, redux persist

---
+++ src/app/store.js
@@ -0,0 +1,33 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import createFilter from "redux-persist-transform-filter";
+import storage from "redux-persist/lib/storage";
+//slices
+import userSlice from "../features/userSlice";
+
+//saveUserOnlyFilter
+const saveUserOnlyFilter = createFilter("user", ["user"]);
+
+//persist config
+const persistConfig = {
+  key: "user",
+  storage,
+  whitelist: ["user"],
+  transforms: [saveUserOnlyFilter],
+};
+
+const rootReducer = combineReducers({
+  user: userSlice,
+});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+  devTools: true,
+});
+
+export const persistor = persistStore(store);
---
+++ src/features/userSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  status: "",
+  error: "",
+  user: {
+    id: "",
+    name: "",
+    email: "",
+    picture: "",
+    status: "",
+    token: "",
+  },
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    logout: (state) => {
+      state.status = "";
+      state.error = "";
+      state.user = {
+        id: "",
+        name: "",
+        email: "",
+        picture: "",
+        status: "",
+        token: "",
+      };
+    },
+  },
+});
+
+export const { logout } = userSlice.actions;
+
+export default userSlice.reducer;
