fb6c812a50ea08333e6cb8b547f164604820118f
MOHAMED HAJJI <pytaek1@gmail.com>
2023-03-29 10:38:23 +0000

    protected routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { io } from "socket.io-client";
 import SocketContext from "./context/SocketContext";
 //Pages
@@ -10,14 +16,30 @@ import Register from "./pages/register";
 const socket = io(process.env.REACT_APP_API_ENDPOINT.split("/api/v1")[0]);
 
 function App() {
+  const { user } = useSelector((state) => state.user);
+  const { token } = user;
   return (
     <div className="dark">
       <SocketContext.Provider value={socket}>
         <Router>
           <Routes>
-            <Route exact path="/" element={<Home socket={socket} />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/register" element={<Register />} />
+            <Route
+              exact
+              path="/"
+              element={
+                token ? <Home socket={socket} /> : <Navigate to="/login" />
+              }
+            />
+            <Route
+              exact
+              path="/login"
+              element={!token ? <Login /> : <Navigate to="/" />}
+            />
+            <Route
+              exact
+              path="/register"
+              element={!token ? <Register /> : <Navigate to="/" />}
+            />
           </Routes>
         </Router>
       </SocketContext.Provider>
